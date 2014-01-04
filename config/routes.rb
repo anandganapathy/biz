6af9a6053109d08820ed64adba0c8cf0708b6ac3
Zeus <anandg@zeustechsolutions.com>
2014-01-04 00:52:32 -0800

    2 repository

--- config/routes.rb
+++ config/routes.rb
@@ -25,7 +25,7 @@ Biz::Application.routes.draw do
 
   
   
-  root to: 'homes#index'
+  root to: 'user_step1s#new'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
